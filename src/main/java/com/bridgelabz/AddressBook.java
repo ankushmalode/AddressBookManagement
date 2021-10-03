3545edd194433456287d8b46297066fee7da0115
ankushmalode <you@example.com>
2021-10-03 16:09:10 +0530

    Ability to add contacts in address book

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -1,22 +1,47 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBook {
+    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
-        Scanner sc = new Scanner(System.in);
-        int menu;
 
-        System.out.println(" 0. Exit. ");
-        System.out.println(" 1. Add contact. ");
-        System.out.println(" 2. Edit contact. ");
-        System.out.println(" 3. display contact. ");
-        System.out.println(" 4. Delete contact. ");
-        menu = sc.nextInt();
-        if (menu>=0 && menu<5)
-            System.out.println("You have selected option is "+menu);
-        else
-            System.out.println("You have selected wrong input.");
+        AddressBook addressBook = new AddressBook();
+        addressBook.askUser();
+    }
+
+    /*Menu
+         */
+        void askUser () {
+            System.out.println(" Select the action ");
+            System.out.println("1. Add contact.\n2. Edit contact. \n3. display contact. \n4. Delete contact. \n5. Exit");
+
+            int choice =scanner.nextInt();
+            switch (choice){
+                case 1:
+                    addPerson();
+                    break;
+            }
+        }
+        /* user info
+         */
+    void addPerson(){
+        System.out.print("Enter first name ");
+        String firstName=scanner.next();
+        System.out.print("Enter Last name ");
+        String lastName=scanner.next();
+        System.out.print("Enter address: ");
+        String address=scanner.nextLine();
+        System.out.print("Enter city, state: ");
+        String city=scanner.next();
+        String state=scanner.next();
+        System.out.print("Enter PIN code: ");
+        String pinCode=scanner.next();
+        System.out.print("Enter phone number: ");
+        String phoneNumber=scanner.next();
+        System.out.print("Enter email ID: ");
+        String emaiID= scanner.next();
     }
 }
