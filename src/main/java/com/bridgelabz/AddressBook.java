d47c6ad8c4bfa73998039a070807da2af61d3cdf
ankushmalode <you@example.com>
2021-10-03 18:25:04 +0530

    edit person details

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -13,20 +13,23 @@ public class AddressBook {
     }
 
     /*Menu
-         */
-        void askUser () {
-            System.out.println(" Select the action ");
-            System.out.println("1. Add contact.\n2. Edit contact. \n3. display contact. \n4. Delete contact. \n5. Exit");
+     */
+    void askUser () {
+        System.out.println(" Select the action ");
+        System.out.println("1. Add contact.\n2. Edit contact. \n3. display contact. \n4. Delete contact. \n5. Exit");
 
-            int choice =scanner.nextInt();
-            switch (choice){
-                case 1:
-                    addPerson();
-                    break;
-            }
+        int choice =scanner.nextInt();
+        switch (choice){
+            case 1:
+                addPerson();
+                break;
+            case 2:
+                System.out.println("Enter first and last name of the person to edit the contact: ");
+                break;
         }
-        /* user info
-         */
+    }
+    /* user info
+     */
     void addPerson(){
         System.out.print("Enter first name ");
         String firstName=scanner.next();
@@ -43,5 +46,38 @@ public class AddressBook {
         String phoneNumber=scanner.next();
         System.out.print("Enter email ID: ");
         String emaiID= scanner.next();
+    }
+    void edit(){
+        System.out.print("What do you want to edit:");
+        System.out.println("1. First name: \n2. Last Name: \n3. Address: \n4. City\n5. State\n6. phone number\n7. email\n8. PIN Code");
+        int choice=scanner.nextInt();
+        switch (choice){
+            case 1:
+                String firstName=scanner.next();
+                break;
+            case 2:
+                String lastName=scanner.next();
+                break;
+            case 3:
+                String address=scanner.nextLine();
+                break;
+            case 4:
+                String city=scanner.next();
+                break;
+            case 5:
+                String state=scanner.next();
+                break;
+            case 6:
+                String phoneNumber=scanner.next();
+                break;
+            case 7:
+                String emaiID= scanner.next();
+                break;
+            case 8:
+                String pinCode=scanner.next();
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ src/main/java/com/bridgelabz/Person.java
@@ -0,0 +1,6 @@
+package com.bridgelabz;
+
+public class Person {
+    public JSONObject toJsonObject() {
+    }
+}
